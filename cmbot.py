b1a053a825910efcb9f324ffd2a79368367df31f
CoinMaker <35696423+CoinsMaker@users.noreply.github.com>
2018-01-30 03:48:35 +1000

    initial

--- cmbot.py
+++ cmbot.py
@@ -6,41 +6,41 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['help'])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришла команда /help" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришла команда /help")
 
 @bot.message_handler(commands=['start'])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришла команда /start" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришла команда /start")
 
 @bot.message_handler(commands=['settings'])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришла команда /settings" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришла команда /settings")
 
 @bot.message_handler(commands=['action'])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришла команда /action" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришла команда /action")
 
 
 
 @bot.message_handler(content_types=["text"] + message.from_user.id)
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришло простое сообщение" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришло простое сообщение")
 
 @bot.message_handler(content_types=["document"])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришел документ" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришел документ")
 
 @bot.message_handler(content_types=["audio"])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришла аудиозапись" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришла аудиозапись")
 
 @bot.message_handler(content_types=["photo"])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришло изображение" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришло изображение")
 
 @bot.message_handler(content_types=["sticker"])
 def handle_text(message):
-    bot.send_message(message.from_user.id, "Пришел стикер" + message.from_user.id)
+    bot.send_message(message.from_user.id, "Пришел стикер")
 
 
 
